1bc8bde897d998210767931b23e2d07d3d49f39c
Tcreamer <trentoncreamer@yahoo.com>
2020-08-26 19:20:37 -0700

    mongoose databases laid out and collections created

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+require("./models");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect("mongodb://localhost/google_books", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
